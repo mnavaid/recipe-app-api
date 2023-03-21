b21dfb2cf162a3206992b3133b72fbf175eb2828
Mohd Navaid Akhtar1 <mohd.navaid.akhtar1@ibm.com>
2023-03-21 13:03:51 +0530

    commiting the changes on 32123

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+  Calculator functions  
+"""
+
+def add(x,y):
+
+    """ Add two numbers and return the results"""
+    return x+y
+
+def substract(x,y):
+    """ Substrations"""
+    return y-x
